60de0b5d705eb64360377f7604b4a0470938e383
Vicman8 <Victor2044usa@gmail.com>
2023-11-13 15:41:54 -0500

    Tried Fixing bottles and made runup and rundown animations for player

--- Assets/Scripts/Bottleshoot.cs
+++ Assets/Scripts/Bottleshoot.cs
@@ -7,19 +7,19 @@ public class Bottleshoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "bullet")
-        {
+        { 
             Debug.Log("contact");
             Destroy(gameObject);
         }
