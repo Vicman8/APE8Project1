dcc04320ffcec8dcdd0a0acbade854b09b7a04d2
Vicman8 <Victor2044usa@gmail.com>
2023-12-04 16:01:31 -0500

    Made it so you press E to change scene

--- Assets/Scripts/OutsideSceneChange.cs
+++ Assets/Scripts/OutsideSceneChange.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OutsideSceneChange : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class OutsideSceneChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Board");
+        }
     }
 }
