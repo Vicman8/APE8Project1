e9e3498f47377255d8fe18cc61b3f50392c3b6cd
GalacticSeal <spacesealsgobrrrrrrr@gmail.com>
2023-12-06 15:43:30 -0500

    Broken CreateAOE code, Broken DamageRaycast code
    Neither script seems to be working as intended for no clear reason and cannot be fixed even after extensively looking over the code and reworking parts of it. No further attempt to make it work will be done due to time constraints and no obvious problem being evident.

--- Assets/Scripts/DamageRaycast.cs
+++ Assets/Scripts/DamageRaycast.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class DamageRaycast : MonoBehaviour
 {
+    private LayerMask mask;
+    private void Start() {
+        mask = LayerMask.GetMask("PlayerHitbox");
+    }
+
     public void HitboxRect(Vector2 origin, float distance, float width, float zAngle, int damage = 1) {
-        CheckHit(Physics2D.BoxCast(origin, new Vector2(width, distance), zAngle, new Vector2(0,-1), Mathf.Infinity, 6), damage);
+        CheckHit(Physics2D.OverlapBox(origin, new Vector2(width, distance), zAngle, mask), damage);
     }
     public void HitboxRect(Vector2 origin, Vector2 dimensions, float zAngle, int damage = 1) {
-        CheckHit(Physics2D.BoxCast(origin, dimensions, zAngle, new Vector2(0,-1), Mathf.Infinity, 6), damage);
+        CheckHit(Physics2D.OverlapBox(origin, dimensions, zAngle, mask), damage);
     }
 
 
     public void HitboxCircle(CircleCollider2D hitbox, int damage = 1) {
-        CheckHit(Physics2D.CircleCast(hitbox.bounds.center, hitbox.radius, transform.up, Mathf.Infinity, 6), damage);
+        CheckHit(Physics2D.CircleCast(hitbox.bounds.center, hitbox.radius, transform.up, Mathf.Infinity, mask), damage);
     }
     public void HitboxCircle(Vector2 center, float radius, int damage = 1) {
-        CheckHit(Physics2D.CircleCast(center, radius, Vector2.zero, Mathf.Infinity, 6), damage);
+        CheckHit(Physics2D.CircleCast(center, radius, Vector2.zero, Mathf.Infinity, mask), damage);
     }
 
 
+    private bool CheckHit(Collider2D hit, int damagePoints) {
+        if(hit.GetComponent<PlayerLose>() != null) {
+            hit.GetComponent<PlayerLose>().ReceiveDamage(damagePoints); //only player hitbox is on mask layer 6
+            return true;
+        }
+        return false;
+    }
     private bool CheckHit(RaycastHit2D hit, int damagePoints) {
         if(hit.collider.GetComponent<PlayerLose>() != null) {
             hit.collider.GetComponent<PlayerLose>().ReceiveDamage(damagePoints); //only player hitbox is on mask layer 6
